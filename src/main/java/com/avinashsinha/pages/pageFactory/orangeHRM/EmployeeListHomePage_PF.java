654b133587a8657864980eb26ba96865f1a5730d
Avinash Sinha <sinha.avinash906@gmail.com>
2026-08-29 14:09:52 +0530

    Selenium Advance Framework

--- src/main/java/com/avinashsinha/pages/pageFactory/orangeHRM/EmployeeListHomePage_PF.java
+++ src/main/java/com/avinashsinha/pages/pageFactory/orangeHRM/EmployeeListHomePage_PF.java
@@ -1,32 +1,38 @@
 package com.avinashsinha.pages.pageFactory.orangeHRM;
 
 import com.avinashsinha.base.CommonToAllPage;
+import com.avinashsinha.utils.WaitHelpers;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 
-import static com.avinashsinha.utils.WaitHelpers.waitJVM;
-
 //This is Page Class
 public class EmployeeListHomePage_PF extends CommonToAllPage {
 
+    private static final Logger LOGGER = LogManager.getLogger(EmployeeListHomePage_PF.class);
+
     WebDriver driver;
 
-    public EmployeeListHomePage_PF(WebDriver driver){
-        this.driver=driver;
-        PageFactory.initElements(driver,this);
+    public EmployeeListHomePage_PF(WebDriver driver) {
+        this.driver = driver;
+        PageFactory.initElements(driver, this);
     }
 
     //Step 1 : These are Page Locators i.e. Kind of Attributes or Instance Variable or Member Variable
     @FindBy(xpath = "//h6[normalize-space()='Dashboard']")
     private WebElement userNameOnDashboard;
 
-    //Step 2 : These are Page Actions i.e. Kind of Behaviours or Instance Methods or Member Methods
+    //Step 2 : These are Page Actions i.e. Kind of Behaviors or Instance Methods or Member Methods
     public String loggedInUserName() {
 
-        waitJVM(5000);
-        return getText(userNameOnDashboard);
+        WaitHelpers.visibilityOfElement(driver, userNameOnDashboard);
+        String titleText = getText(userNameOnDashboard);
+        LOGGER.info("Dashboard Page Title: {}", titleText);
+
+        return titleText;
 
     }
 
