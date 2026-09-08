ac89faac708b44765c17076039aea501022648c8
Avinash Sinha <sinha.avinash906@gmail.com>
2026-09-08 22:37:06 +0530

    Selenium Advance Framework

--- src/main/java/com/avinashsinha/pages/pageFactory/orangeHRM/LoginPage_PF.java
+++ src/main/java/com/avinashsinha/pages/pageFactory/orangeHRM/LoginPage_PF.java
@@ -3,6 +3,8 @@ package com.avinashsinha.pages.pageFactory.orangeHRM;
 import com.avinashsinha.base.CommonToAllPage;
 import com.avinashsinha.utils.PropertiesReader;
 import com.avinashsinha.utils.WaitHelpers;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
@@ -10,6 +12,8 @@ import org.openqa.selenium.support.PageFactory;
 
 //This is Page Class
 public class LoginPage_PF extends CommonToAllPage {
+
+    private static final Logger LOGGER = LogManager.getLogger(LoginPage_PF.class);
 
     WebDriver driver;
 
@@ -28,18 +32,21 @@ public class LoginPage_PF extends CommonToAllPage {
     @FindBy(xpath = "//button[normalize-space()='Login']")
     private WebElement submitButton;
 
-    //Step 2 : These are Page Actions i.e. Kind of Behaviours or Instance Methods or Member Methods
+    //Step 2 : These are Page Actions i.e. Kind of Behaviors or Instance Methods or Member Methods
     public void loginToOrangeHRMCreds() {
 
         openOrangeHRMUrl();
 
-        WaitHelpers.waitJVM(5000);
+        WaitHelpers.visibilityOfElement(username);
+        LOGGER.info("Login form loaded. Entering credentials.");
 
         enterInput(username, PropertiesReader.readKey("ohr_username"));
         enterInput(password, PropertiesReader.readKey("ohr_password"));
         clickElement(submitButton);
 
         WaitHelpers.waitJVM(5000);
+
+        LOGGER.info("Login submitted.");
 
     }
 
