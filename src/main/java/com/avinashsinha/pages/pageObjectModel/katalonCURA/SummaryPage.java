b7bdb38068f9caf2be8ffbb3a6313d2bc6c21469
Avinash Sinha <sinha.avinash906@gmail.com>
2026-09-18 23:17:19 +0530

    Selenium Advance Framework

--- src/main/java/com/avinashsinha/pages/pageObjectModel/katalonCURA/SummaryPage.java
+++ src/main/java/com/avinashsinha/pages/pageObjectModel/katalonCURA/SummaryPage.java
@@ -3,11 +3,15 @@ package com.avinashsinha.pages.pageObjectModel.katalonCURA;
 import com.avinashsinha.base.CommonToAllPage;
 import com.avinashsinha.utils.PropertiesReader;
 import com.avinashsinha.utils.WaitHelpers;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 
 //This is Page Class
 public class SummaryPage extends CommonToAllPage {
+
+    private static final Logger LOGGER = LogManager.getLogger(SummaryPage.class);
 
     WebDriver driver;
 
@@ -22,23 +26,34 @@ public class SummaryPage extends CommonToAllPage {
     private static final By LOGOUT = By.linkText("Logout");
 
     //Step 2 : These are Page Actions i.e. Kind of Behaviors or Instance Methods or Member Methods
-    public void checkAllDetailsOnSummaryPage() {
+    public boolean checkAllDetailsOnSummaryPage() {
 
         WaitHelpers.urlContains(driver, PropertiesReader.readKey("katalon_summaryURL"));
 
-        if (getText(COMMENT_TEXT).equals(PropertiesReader.readKey("katalon_comment"))) {
+        boolean isConfirmationPresent = WaitHelpers.isElementPresent(driver, CONFIRMATION_TEXT);
+        String actualComment = getText(COMMENT_TEXT);
+        boolean isCommentMatching = actualComment.equals(PropertiesReader.readKey("katalon_comment"));
 
-            clickElement(SIDE_BAR);
-            clickElement(LOGOUT);
+        boolean isSummaryValid = isConfirmationPresent && isCommentMatching;
 
-            WaitHelpers.urlContains(driver, PropertiesReader.readKey("katalon_makeAppointmentURL"));
-
-            System.out.println("\nTest Case is Passed\n");
-
+        if (isSummaryValid) {
+            LOGGER.info("Summary Page verified. Confirmation shown and comment matches.");
         } else {
-
-            System.out.println("\nTest Case is Failed");
+            LOGGER.error("Summary Page Failed: confirmationPresent={}, commentMatches={} (expected '{}', actual '{}')",
+                    isConfirmationPresent, isCommentMatching, PropertiesReader.readKey("katalon_comment"), actualComment);
         }
+
+        return isSummaryValid;
+
+    }
+
+    public void logout() {
+
+        clickElement(SIDE_BAR);
+        clickElement(LOGOUT);
+
+        WaitHelpers.urlContains(driver, PropertiesReader.readKey("katalon_makeAppointmentURL"));
+        LOGGER.info("Logged out successfully, returned to Make Appointment page.");
 
     }
 }
